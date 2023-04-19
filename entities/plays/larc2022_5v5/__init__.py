c94f03543212133e3513e5363e53c62260bf7584
Jumelord <thales.machado.fernandes@gmail.com>
2023-04-19 14:07:07 -0300

    removendo plays, estrategias e coachs

--- entities/plays/larc2022_5v5/__init__.py
+++
@@ -1,5 +0,0 @@
-from entities.plays.larc2022_5v5.mainPlay import MainPlay
-
-from entities.plays.larc2022_5v5.penaltyPlay import PenaltyPlay
-
-from entities.plays.larc2022_5v5.freeballPlay import FreeballPlay

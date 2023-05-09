c72a94c7d45f98db87c0c64b7f1d51e11f2810a1
Jumelord <thales.machado.fernandes@gmail.com>
2023-05-09 19:19:30 -0300

    Base coach restored

---
+++ entities/coach/coach.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from os import replace
+
+class BaseCoach(ABC):
+    NAME = "BASE_COACH"
+    def __init__(self, match):
+        self.match = match
+
+    @abstractmethod
+    def decide (self):
+        raise NotImplementedError("Coach needs decide implementation!")
+
+    def get_positions(self, foul, team_color, foul_color, quadrant):
+        return None

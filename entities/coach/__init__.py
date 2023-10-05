b3f82c2801e2b36cf1683bd54b9b06de0e44d80c
Ana-Monici <ana.monici@outlook.com>
2023-10-05 17:44:24 -0300

    Created A* based plays for new midfielder strategy

--- entities/coach/__init__.py
+++ entities/coach/__init__.py
@@ -1,11 +1,13 @@
 from entities.coach.coach import BaseCoach
 from entities.coach.coach_rcx2023 import Coach as RCX2023
+from entities.coach.test_coach import Coach as TEST
 
 #from entities.coach.test_coach import Coach as TestCoach
 
 _coach_list = [
     # Tournament coaches
-    RCX2023
+    RCX2023,
+    TEST
 ]
 
 COACHES = {c.NAME: c for c in _coach_list}
